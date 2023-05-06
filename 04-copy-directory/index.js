9e05c19ba5f54c9dab0609afb3f267f2af4acc0d
Dzmitry Salivon <saleevong@gmail.com>
2023-05-06 12:40:50 +0200

    feat: solved tasks 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,37 @@
+const fs = require("fs");
+const path = require("path");
 
+const pastFolder = path.resolve(__dirname, "files-copy");
+const dataFolder = path.resolve(__dirname, "files");
+
+fs.rm(pastFolder, { recursive: true }, (err) => {
+  if (err) {
+    console.error(err);
+  }
+
+  fs.mkdir(pastFolder, { recursive: true }, (err) => {
+    if (err) {
+      console.error(err);
+    }
+
+    fs.readdir(dataFolder, { withFileTypes: true }, (err, files) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      files.forEach((file) => {
+        fs.copyFile(
+          path.join(dataFolder, file.name),
+          path.join(pastFolder, file.name),
+          (err) => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+          }
+        );
+      });
+    });
+  });
+});
